6baaabb12fe843a3056c796151cf7912b4a84588
gvidas <danieliusgvidas@gmail.com>
2024-10-15 11:57:22 +0100

    matching static patterns

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-static char emptyChar = ' ';
+static char emptyChar = '.';
 static char fullChar = '#';
 
 static int threadCount = 12;
@@ -20,6 +20,8 @@ pair<int, int> directions[8] = {
 void printA(){
     cout << "A";
 }
+
+
 
 class Grid {
 public:
@@ -94,23 +96,6 @@ public:
 
     }
 
-
-    void stepThread(bool* tempGrid, int start, int end){
-        for (int y = 0; y < h; ++y) {
-            for (int x = 0; x < w; ++x) {
-                int cellNumber = y*w + x;
-                tempGrid[cellNumber] = calculateCell(x, y);
-            }
-        }
-
-        for (int i = start; i < end; ++i) {
-            int x = i % w;
-            int y = (i - x) / w;
-            tempGrid[i] = calculateCell(x, y);
-        }
-
-    }
-
     void stepSimulation() {
         bool* tempGrid = new bool[cellCount];
 
@@ -119,7 +104,21 @@ public:
         }
 
 
-
+//        float cellsPerThread = (float) cellCount/threadCount;
+//
+//        vector<thread> threads;
+//
+//        for (int i = 0; i < threadCount; ++i) {
+//            int end = round((i+1) * cellsPerThread);
+//            int start = round(i * cellsPerThread);
+//            int assignedCells = end - start;
+//            cout << assignedCells << "A";
+//            threads.push_back(thread(&Grid::stepThread, this, tempGrid, start, end));
+//        }
+//
+//        for (thread& t: threads) {
+//            t.join();
+//        }
 
         for (int y = 0; y < h; ++y) {
             for (int x = 0; x < w; ++x) {
@@ -189,21 +188,126 @@ private:
 
 };
 
+class BasePattern {
+public:
+    int w;
+    int h;
+    int cellCount;
+    vector<bool> pattern;
 
+    BasePattern(vector<bool> newPat, int width, int height) {
+        pattern = newPat;
+        w = width;
+        h = height;
+        cellCount = w * h;
+    }
+
+    void findInGrid(Grid grid) {
+        for (int y = 0; y < grid.h; ++y) {
+            for (int x = 0; x < grid.w; ++x) {
+
+                int totalMatches = 0;
+                for (int i = 0; i < cellCount; ++i) {
+
+                    int patX = i % w;
+                    int patY = (i - patX) / w;
+
+                    int newX = patX + x;
+                    int newY = patY + y;
+
+                    if (newX < grid.w && newY < grid.h) {
+                        if (grid.grid[newY*grid.w + newX] == pattern[i]){
+                            totalMatches++;
+                        } else {break;}
+                    }
+
+                }
+                if (totalMatches == cellCount) {
+                    totalMatches = 0;
+                    cout << "matching" << x <<" "<< y << " ";
+                    for (int a = -1; a < w+1; ++a) {
+
+                        //top row calculations
+                        bool topRow;
+                        if (y-1 < 0) {
+                            topRow = 0;
+                        } else {
+                            topRow = grid.grid[(y-1)*grid.w + x + a];
+                        }
+
+                        //bottom row calculations
+                        bool bottomRow;
+                        if (y + h >= grid.h) {
+                            bottomRow = 0;
+                        } else {
+                            bottomRow = (grid.grid[(y+h)*grid.w + x + a]);
+                        }
+
+                        if (!topRow && !bottomRow) {
+                            totalMatches++;
+                        } else {
+                            break;
+                        }
+                    }
+
+                    for (int b = 0; b < h; ++b) {
+                        bool leftCol;
+                        if (x-1 < 0) {
+                            leftCol = 0;
+                        } else {
+                            leftCol = grid.grid[(y+b)*grid.w + x - 1];
+                        }
+                        bool rightCol;
+                        if (x-1 < 0) {
+                            rightCol = 0;
+                        } else {
+                            rightCol = grid.grid[(y+b)*grid.w + x + w];
+                        }
+                        cout << "| x:" << x << " y:" << y << " b:"  << b << " "<< leftCol << " " << rightCol << "|";
+                        if (!leftCol && !rightCol) {
+                            totalMatches++;
+                        } else {
+                            break;
+                        }
+                    }
+                    
+                    cout << "."<< totalMatches << " " << (w+2)*2 << endl;
+                    if (totalMatches == w+2 + h) {
+                        cout << "MATCH: " << x << " " << y;
+                    }
+                    cout << endl;
+                }
+            }
+        }
+
+    }
+private:
+};
 
 int main() {
     //setup
     srand(2);
     Grid grid = Grid();
 
+
     // if a grid is loaded;
     bool simulating = false;
+
+
+    vector<bool> arr = {1,1,1,1};
+    BasePattern block = BasePattern(arr, 2, 2);
+
+    arr = {0,1,0,
+           1,0,1,
+           1,0,1,
+           0,1,0};
+    BasePattern beehive = BasePattern(arr, 3,4);
 
 
     while (true) {
         string input;
         if (simulating) {
-            cout << "cmds: step, save, exit: ";
+            cout << "cmds: step, save, match, exit: ";
             cin >> input;
             if (input.substr(0,4) == "exit") {
                 simulating = false;
@@ -225,7 +329,11 @@ int main() {
                 auto ms_int = chrono::duration_cast<chrono::milliseconds>(t2-t1);
                 cout << ms_int.count() << "ms."<< endl;
 
-                //grid.printGrid();
+                grid.printGrid();
+            }
+            else if (input.substr(0,5) == "match") {
+                block.findInGrid(grid);
+                beehive.findInGrid(grid);
             }
 
         } else {
@@ -245,7 +353,7 @@ int main() {
                 grid.initGrid(x, y);
                 grid.randomiseGrid(cellAmt);
 
-                //grid.printGrid();
+                grid.printGrid();
 
                 simulating = true;
             }
